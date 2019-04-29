81af3048dba3c5a414d1cb3668136a191830b18d
Mengwei Ding <mengwei.ding@gmail.com>
2019-04-29 11:17:07 +0800

    Override the options for Worktree.add

---
+++ test/tests/worktree.js
@@ -0,0 +1,49 @@
+var path = require("path");
+var assert = require("assert");
+var fse = require("fs-extra");
+var local = path.join.bind(path, __dirname);
+
+describe("Worktree", function() {
+  var NodeGit = require("../../");
+  var Repository = NodeGit.Repository;
+  var Worktree = NodeGit.Worktree;
+  var Clone = NodeGit.Clone;
+
+  var clonePath = local("../repos/clone");
+  var worktreePath = local("../repos/worktree");
+
+  // Set a reasonable timeout here now that our repository has grown.
+  this.timeout(30000);
+
+  before(function() {
+    var test = this;
+    var url = "https://github.com/nodegit/test.git";
+    var opts = {
+        fetchOpts: {
+          callbacks: {
+            certificateCheck: () => 0
+        }
+      }
+    };
+
+    return Clone(url, clonePath, opts).then(function(repo) {
+      assert.ok(repo instanceof Repository);
+      test.repository = repo;
+    });
+  });
+
+  after(function() {
+    return fse.remove(clonePath).catch(function(err) {
+      console.log(err);
+
+      throw err;
+    });
+  });
+
+  it("can create worktree", function() {
+    return Worktree.add(this.repository, "workspace", worktreePath, {})
+      .then(function(wt) {
+        assert.ok(wt instanceof Worktree);
+      });
+  });
+});
